bf300c34ce931a2a581360893f7fbc0e594d60d1
gingerman14 <davedeckman14@gmail.com>
2024-12-19 15:55:50 -0800

    character count sorter and refined outputs

--- main.py
+++ main.py
@@ -3,7 +3,17 @@ def main():
     text = get_book_text(book_path)
     word_count = get_word_count(text)
     char_count = get_char_count(text)
-    print(char_count)
+    char_list = sorted_character_count(char_count)
+
+    print(f"---Begin report of {book_path} ---")
+    print(f"{word_count} words found in the document")
+    print()
+
+    for item in char_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"The '{item['char']}' character was fount {item['num']} time")
+    print("--- End report ---")
 
 def get_book_text(path):
         with open(path) as f:
@@ -22,5 +32,18 @@ def get_char_count(text):
         else:
             char[letters] = 1
     return char
+
+def sort_on(d):
+    return d["num"]
+
+def sorted_character_count(text):
+    char_dict = get_char_count(text)
+    char_list = []
+    for d in char_dict:
+        char_list.append({"char": d, "num": text[d]})
+    char_list.sort(reverse=True, key=sort_on)
+    return char_list
+
+    
 
 main()
